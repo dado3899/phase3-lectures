1e1baba0b26c3d6dfe22597341c337a5e4d0f3e9
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-07-24 14:07:03 -0600

    solution

---
+++ test.py
@@ -0,0 +1,28 @@
+arr = [0,1,2,3,5,2,4,0,1,2,3,5,2,4,0,1,2,3,5,2,4,0,1,2,3,5,2,4,0,1,2,3,5,2,4,20]
+arrsorted = [0,1,2,3,5,6,7,8,9,10,11,12,13,14]
+target = 27
+
+for i in arrsorted:
+    # cutting in half logic
+    pass
+# len(arr) = n
+# however big arr is
+def findNum(find):
+    if find < 0:
+        return False
+    1
+    # n
+    for num in arr: #2n
+        if num == find:
+            return num 
+        else:
+            print("hello")
+    # n^2
+    for num in arr: #2n
+        for num2 in arr:
+            pass
+    if find in arr:
+        return True
+        
+
+O(n)
